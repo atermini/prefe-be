31d2225f0f51dc00a76c848f5613f7840271eb9b
Andrea Termini <andrea.termini3@gmail.com>
2026-05-01 00:35:39 +0200

    fixed

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'Europe/Rome'),
+    'timezone' => 'Europe/Rome',
 
     /*
     |--------------------------------------------------------------------------
