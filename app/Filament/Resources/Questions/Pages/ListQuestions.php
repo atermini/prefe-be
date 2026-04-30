448266d6522d36e32f87808b5da73301a990a074
Andrea Termini <andrea.termini3@gmail.com>
2026-05-01 00:42:39 +0200

    update

--- app/Filament/Resources/Questions/Pages/ListQuestions.php
+++ app/Filament/Resources/Questions/Pages/ListQuestions.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\Questions\Pages;
 
+use App\Actions\GenerateDailyQuestion;
 use App\Filament\Resources\Questions\QuestionResource;
+use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
+use Throwable;
 
 class ListQuestions extends ListRecords
 {
@@ -13,6 +17,30 @@ class ListQuestions extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('generateDailyQuestion')
+                ->label('Genera domanda di oggi')
+                ->icon('heroicon-o-sparkles')
+                ->color('warning')
+                ->requiresConfirmation()
+                ->modalHeading('Genera domanda di oggi')
+                ->modalDescription('Verrà chiamata l\'AI per generare la domanda del giorno. Se esiste già una domanda per oggi verrà sovrascritta.')
+                ->modalSubmitActionLabel('Genera')
+                ->action(function (): void {
+                    try {
+                        (new GenerateDailyQuestion)->handle(force: true);
+
+                        Notification::make()
+                            ->title('Domanda generata con successo')
+                            ->success()
+                            ->send();
+                    } catch (Throwable $e) {
+                        Notification::make()
+                            ->title('Errore durante la generazione')
+                            ->body($e->getMessage())
+                            ->danger()
+                            ->send();
+                    }
+                }),
             CreateAction::make(),
         ];
     }
